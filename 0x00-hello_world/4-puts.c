1555a4bcdff4cb44bd2f2843102074df92bdd071
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2023-02-16 13:58:18 -0800

    kjfh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
