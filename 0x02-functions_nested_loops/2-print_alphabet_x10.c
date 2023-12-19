8c88977a0105d7b2952a5e88d1d7b5f16a8a1d1a
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2023-12-19 13:17:59 +0200

    Update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
-		print_alphabet();
+		for (j = 97 ; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+
+		_putchar('\n');
 	}
 }
