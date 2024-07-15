3026b8b02e9bc6b9200dcfe9ba9dca8a0eb83f61
abuabdelrahman1413 <mss2abuabdelrahman@gmail.com>
2024-07-15 15:05:19 +0200

    task 2

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -29,12 +29,7 @@ int linear_search(int *array, size_t size, int value)
 }
 
 
-/**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
- */
-int main(void)
+/*int main(void)
 {
     int array[] = {
         10, 1, 42, 3, 4, 42, 6, 7, -1, 9
@@ -45,4 +40,4 @@ int main(void)
     printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
     printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
     return (EXIT_SUCCESS);
-}
+}*/
